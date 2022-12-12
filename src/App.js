73a261e2c144846ba6b30de490895bdf043ab94d
brandon <srlguitarist@gmail.com>
2022-12-11 22:51:19 -0500

    Add stars background video

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import "./App.css";
-import data from "./data.json";
+import starsVideo from "./stars.mp4";
 
 function App() {
   return (
     <div className="App">
+      <video autoPlay muted loop id="stars-background">
+        <source src={starsVideo} type="video/mp4" />
+      </video>
       <Button variant="contained" color="primary">
         Hello World
       </Button>
-      {Object.values(data).map((item) => (
-        <h1>{item}</h1>
-      ))}
     </div>
   );
 }
